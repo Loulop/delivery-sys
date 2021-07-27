9e5fcf97284281e7b8940f2e390808dab9e31d6e
Ali Moallim <amoallim15@gmail.com>
2021-07-27 23:08:59 +0900

    commit 3.1

--- db_schemas/store_schemas.sql
+++ db_schemas/store_schemas.sql
@@ -7,14 +7,21 @@ CREATE TABLE IF NOT EXISTS store (
     --
     user_id                     VARCHAR(128) NOT NULL,
     name                        VARCHAR(256) NOT NULL,
-    store_status                BOOLEAN NOT NULL,      -- offline, online
-    -- 
+    store_service_status        BOOLEAN NOT NULL,      -- offline, online
+    city_id                     VARCHAR(128) NOT NULL,
     -- TODO
     location_lat                DOUBLE NULL,
     location_lng                DOUBLE NULL,
+    address                     VARCHAR(256) NULL,
+    address_2                   VARCHAR(256) NULL,
+    postal_code                 VARCHAR(256) NULL,  
+    contact_emails              VARCHAR(512) NULL,
+    offline_reason              VARCHAR(128) NULL,      -- OUT_OF_MENU_HOURS, INVISIBLE, PAUSED_BY_OUR_APP, PAUSED_BY_STORE
+    external_store_id           VARCHAR(128) NULL,
     -- 
     PRIMARY KEY (store_id)
     -- FOREIGN KEY (user_id)
+    -- FOREIGN KEY (city_id)
 );
 
 -- Market @@ <-----> @@ Store
@@ -31,7 +38,26 @@ CREATE TABLE IF NOT EXISTS market_stores (
     market_id           VARCHAR(128) NOT NULL,
     store_id            VARCHAR(128) NOT NULL,
     -- 
-    -- FOREIGN KEY (market_id),
-    -- FOREIGN KEY (store_id),
+    -- FOREIGN KEY (market_id)
+    -- FOREIGN KEY (store_id)
     INDEX (market_id)
+);
+
+CREATE TABLE IF NOT EXISTS cites (
+    city_id                     VARCHAR(128) NOT NULL,
+    -- 
+    name                        VARCHAR(218) NOT NULL,
+    country_code                VARCHAR(3) NOT NULL,
+    -- 
+    PRIMARY KEY (city_id)
+);
+
+-- this table is better if it is managed by mongodb or etc.
+CREATE TABLE IF NOT EXISTS store_stats (
+    store_id                    VARCHAR(128) NOT NULL,
+    -- 
+    -- 
+    average_prep_time           INT UNSIGNED NULL, 
+    -- 
+    PRIMARY KEY (store_id)
 );
